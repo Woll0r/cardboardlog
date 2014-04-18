dd673b6ff1c5addf3dafa967435b638e3cb3535d
Wolfgang Vandevenne <woll0r@gmail.com>
2014-04-18 18:09:25 +0200

    Import the plugins, fix the shebangs and routes

--- cardboardlog.py
+++ cardboardlog.py
@@ -1,17 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import bottle
+import bottle.ext.sqlite
 
 app = bottle.Bottle()
 plugin = bottle.ext.sqlite.Plugin(dbfile='/home/wolfgang/cardboardenv/cardboardbot/cardboardlog.db')
 app.install(plugin)
 
-@route('/')
-@route('/log/<limit:int>')
+@app.route('/')
+@app.route('/log/<limit:int>')
 def log(limit=100):
     data = db_select("SELECT timestamp, name, message FROM cardboardlog ORDER BY timestamp DESC LIMIT ?", limit)
     output = template('loglist', rows=data)
     return output
 
-@route('/links/<limit>')
+@app.route('/links/<limit>')
 def links(limit=100):
 	data = db_select("SELECT timestamp, name, url, title FROM cardboardlinks ORDER BY timestamp DESC LIMIT ?", limit)
 	output = template('linklist', rows=data)
