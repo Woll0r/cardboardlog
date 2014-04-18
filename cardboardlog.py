4633c6f6e53b22f5932fd0b8bf8848fb925227ec
Wolfgang Vandevenne <woll0r@gmail.com>
2014-04-18 18:22:32 +0200

    Didn't want those variable limits anyway

--- cardboardlog.py
+++ cardboardlog.py
@@ -9,20 +9,20 @@ app = bottle.Bottle()
 @app.route('/')
 @app.route('/log/<limit:int>')
 def log(limit=100):
-    data = db_select("SELECT timestamp, name, message FROM cardboardlog ORDER BY timestamp DESC LIMIT ?", limit)
+    data = db_select("SELECT timestamp, name, message FROM cardboardlog ORDER BY timestamp DESC LIMIT 100")
     output = template('loglist', rows=data)
     return output
 
 @app.route('/links/<limit>')
 def links(limit=100):
-	data = db_select("SELECT timestamp, name, url, title FROM cardboardlinks ORDER BY timestamp DESC LIMIT ?", limit)
+	data = db_select("SELECT timestamp, name, url, title FROM cardboardlinks ORDER BY timestamp DESC LIMIT 100")
 	output = template('linklist', rows=data)
 	return output
 	
-def db_select(query, limit):
+def db_select(query):
     db = sqlite3.connect('/home/wolfgang/cardboardenv/cardboardbot/cardboardlog.db')
     c = db.cursor()
-    c.execute(query, (limit))
+    c.execute(query)
     data = c.fetchall()
     c.close()
     return data
