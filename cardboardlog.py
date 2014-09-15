8c032e0fcfe569543fe142bc1d089bb1c6e3497a
Wolfgang Vandevenne <woll0r@gmail.com>
2014-09-15 04:34:15 +0200

    Allow 0 hours for all time messages

--- cardboardlog.py
+++ cardboardlog.py
@@ -14,6 +14,7 @@ app = application = bottle.Bottle()
 
 db = cardboarddata.CardboardData(dbpath)
 
+
 @app.route('/hook', method='POST')
 def hook():
     data = bottle.request.json
@@ -21,11 +22,13 @@ def hook():
         bottle.abort(400, "Didn't receive proper input.")
     if bottle.request.get_header('X-Github-Event') is None:
         bottle.abort(400, "You're not someone I want to talk to.")
-    print "New commit detected by {} with message {}".format(data['commits'][0]['author']['name'], data['commits'][0]['message'])
+    print "New commit detected by {} with message {}".format(data['commits'][0]['author']['name'],
+                                                             data['commits'][0]['message'])
 
     proc = subprocess.Popen(['sh', 'hook.sh'])
 
     return 'OK!'
+
 
 @app.route('/')
 def index():
@@ -34,7 +37,7 @@ def index():
     messagecount = db.get_log_counts()
     linkscount = db.get_link_counts()
     cardboardbotmessagecount = db.get_link_counts(user='cardboardbot@friendshipismagicsquad.com')
-    linkspercentage = "{0:.2f}".format((linkscount/float(messagecount))*100)
+    linkspercentage = "{0:.2f}".format((linkscount / float(messagecount)) * 100)
     output = bottle.template('index',
                              logs=logs,
                              links=links,
@@ -43,6 +46,7 @@ def index():
                              cardboardbotmessagecount=cardboardbotmessagecount,
                              linkpercentage=linkspercentage)
     return output
+
 
 @app.route('/log2')
 def log2():
@@ -55,6 +59,7 @@ def log2():
     nicks = db.get_users()
     output = bottle.template('logs', data=data, nicks=nicks)
     return output
+
 
 @app.route('/links2')
 def links2():
@@ -69,19 +74,22 @@ def links2():
     output = bottle.template('links', data=data, nicks=nicks)
     return output
 
+
 @app.route('/log')
 @app.route('/log/<limit:int>')
 def log(limit=20):
     page = '/logdata/' + str(limit)
     output = bottle.template('refresh_page', title='CardboardBot Logs', page=page, refreshrate=10000)
     return output
-    
+
+
 @app.route('/links')
 @app.route('/links/<limit:int>')
 def links(limit=20):
     page = '/linksdata/' + str(limit)
     output = bottle.template('refresh_page', title='CardboardBot Links', page=page, refreshrate=60000)
     return output
+
 
 @app.route('/stats')
 def stats():
@@ -90,7 +98,7 @@ def stats():
     cardboardbotmessagecount = db.get_log_counts(user='cardboardbot@friendshipismagicsquad.com')
     messages = db.get_users_by_messages(limit=True)
     links = db.get_users_by_links(limit=True)
-    linkpercentage = "{0:.2f}".format((linkscount/float(messagecount))*100)
+    linkpercentage = "{0:.2f}".format((linkscount / float(messagecount)) * 100)
     messagelinkratio = db.get_users_by_message_link_ratio(limit=True)
     domains = db.get_domains_by_links(limit=True)
     output = bottle.template('stats',
@@ -104,26 +112,30 @@ def stats():
                              domains=domains)
     return output
 
+
 @app.route('/stats/messages')
 def stats_messages():
     messagecount = db.get_log_counts()
     messages = db.get_users_by_messages()
     output = bottle.template('messagestats', messages=messages, messagecount=messagecount)
     return output
-    
+
+
 @app.route('/stats/links')
 def stats_links():
     links = db.get_users_by_links()
     linkcount = db.get_link_counts()
     output = bottle.template('linkstats', links=links, linkcount=linkcount)
     return output
-    
+
+
 @app.route('/stats/ratio')
 def stats_ratio():
     messagelinkratio = db.get_users_by_message_link_ratio()
     output = bottle.template('ratiostats', messagelinkratio=messagelinkratio)
     return output
-    
+
+
 @app.route('/stats/domains')
 def stats_domains():
     domains = db.get_domains_by_links()
@@ -131,11 +143,13 @@ def stats_domains():
     output = bottle.template('domainsstats', domains=domains, linkcount=linkcount)
     return output
 
+
 @app.route('/stats/user')
 def stats_user():
     users = db.get_users()
     output = bottle.template('userstatsselect', users=users)
     return output
+
 
 @app.route('/stats/user/<user>')
 def stats_user(user=None):
@@ -144,24 +158,27 @@ def stats_user(user=None):
     messagecount = db.get_log_counts(user)
     domains = db.get_domains_by_links(limit=False, user=user)
     output = bottle.template('userstats',
-                            nick=nick,
-                            linkcount=linkcount,
-                            messagecount=messagecount,
-                            domains=domains,
-                            user=user)
+                             nick=nick,
+                             linkcount=linkcount,
+                             messagecount=messagecount,
+                             domains=domains,
+                             user=user)
     return output
-    
+
+
 @app.route('/logdata/<limit:int>')
 def logdata(limit=20):
     logdata = db.get_messages(limit)
     output = bottle.template('logdata', data=logdata)
     return output
 
+
 @app.route('/linksdata/<limit:int>')
 def linksdata(limit=20):
     linkdata = db.get_links(limit)
     output = bottle.template('linkdata', data=linkdata)
     return output
+
 
 @app.route('/statsdata')
 def statsdata():
@@ -171,12 +188,14 @@ def statsdata():
     domains = db.get_domains_by_links(limit=True)
     data = dict(messages=messages, links=links, ratio=ratio, domains=domains)
     return data
-    
+
+
 @app.route('/statsdata/messages')
 def statsdata_messages():
     messages = db.get_users_by_messages()
     data = dict(messages=messages)
     return data
+
 
 @app.route('/statsdata/links')
 def statsdata_links():
@@ -184,11 +203,13 @@ def statsdata_links():
     data = dict(links=links)
     return data
 
+
 @app.route('/statsdata/ratio')
 def statsdata_ratio():
     ratio = db.get_users_by_message_link_ratio()
     data = dict(ratio=ratio)
     return data
+
 
 @app.route('/statsdata/domains')
 def statsdata_domains():
@@ -196,16 +217,17 @@ def statsdata_domains():
     data = dict(domains=domains)
     return data
 
+
 @app.route('/statsdata/user/<user>')
 def statsdata_user(user):
     domains = db.get_domains_by_links(user=user)
     data = dict(domains=domains)
     return data
-    
+
+
 @app.route('/static/<filepath:path>')
 def server_static(filepath):
     return bottle.static_file(filepath, root=staticroot)
-    
 
 
 if __name__ == '__main__':
--- modules/cardboarddata.py
+++ modules/cardboarddata.py
@@ -4,69 +4,84 @@
 import sqlite3
 import time
 
+
 class CardboardData():
     def __init__(self, dbpath):
-        self.dbpath=dbpath
+        self.dbpath = dbpath
 
-    def get_messages2(self, hours, user=None):
-        query = 'SELECT l.timestamp, n.nick, l.message FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid AND l.timestamp > ?'
-        seconds = hours * 3600
-        now = time.time()
-        timefrom = now - seconds
-        param = (timefrom, )
+    def get_messages2(self, hours=0, user=None):
+        query = 'SELECT l.timestamp, n.nick, l.message FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid'
+        param = ()
+        if hours > 0:
+            query += 'AND l.timestamp > ?'
+            seconds = hours * 3600
+            now = time.time()
+            timefrom = now - seconds
+            param = (timefrom, )
         if user is not None:
-            query = query + ' AND l.name = ?'
+            query += ' AND l.name = ?'
             param = (timefrom, user)
-        query = query + ' ORDER BY l.timestamp DESC, l.id DESC'
+        query += ' ORDER BY l.timestamp DESC, l.id DESC'
         data = self.select(query, param)
         return data
 
-    def get_links2(self, hours, user=None, domain=None):
-        query = 'SELECT l.timestamp, n.nick, l.url, l.title FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid AND l.timestamp > ?'
-        seconds = hours * 3600
-        now = time.time()
-        timefrom = now - seconds
-        param = (timefrom, )
+    def get_links2(self, hours=0, user=None, domain=None):
+        query = 'SELECT l.timestamp, n.nick, l.url, l.title FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid'
+        param = ()
+        if hours > 0:
+            query += 'AND l.timestamp > ?'
+            seconds = hours * 3600
+            now = time.time()
+            timefrom = now - seconds
+            param = (timefrom, )
         if user is not None:
-            query = query + ' AND l.name = ?'
+            query += ' AND l.name = ?'
             param = param + (user, )
         if domain is not None:
-            query = query + ' AND l.domain = ?'
+            query += ' AND l.domain = ?'
             param = param + (domain, )
-        query = query + ' ORDER BY l.timestamp DESC, l.id DESC'
+        query += ' ORDER BY l.timestamp DESC, l.id DESC'
         data = self.select(query, param)
         return data
 
     def get_users(self):
         data = self.select("SELECT jid, nick FROM cardboardnick ORDER BY nick ASC;")
         return data
-        
+
     def get_user(self, user=None):
         data = self.select("SELECT jid, nick FROM cardboardnick WHERE jid=?", param=(user, ))
         return data[0]
-        
+
     def get_messages(self, limit=20):
-        data = self.select("SELECT l.timestamp, n.nick, l.message FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid ORDER BY l.timestamp DESC, l.id DESC LIMIT " + str(limit))
+        data = self.select(
+            "SELECT l.timestamp, n.nick, l.message FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid ORDER BY l.timestamp DESC, l.id DESC LIMIT " + str(
+                limit))
         data.reverse()
         return data
-        
+
     def get_links(self, limit=20):
-        data = self.select("SELECT l.timestamp, n.nick, l.url, l.title FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid ORDER BY l.timestamp DESC, l.id DESC LIMIT " + str(limit))
+        data = self.select(
+            "SELECT l.timestamp, n.nick, l.url, l.title FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid ORDER BY l.timestamp DESC, l.id DESC LIMIT " + str(
+                limit))
         data.reverse()
         return data
 
     def get_users_by_messages(self, limit=False):
         if limit:
-            data = self.select("SELECT n.nick, COUNT(l.message) AS count FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC LIMIT 10")
+            data = self.select(
+                "SELECT n.nick, COUNT(l.message) AS count FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC LIMIT 10")
         else:
-            data = self.select("SELECT n.nick, COUNT(l.message) AS count FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC")
+            data = self.select(
+                "SELECT n.nick, COUNT(l.message) AS count FROM cardboardlog l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC")
         return data
 
     def get_users_by_links(self, limit=False):
         if limit:
-            data = self.select("SELECT n.nick, COUNT(l.url) AS count FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC LIMIT 10")
+            data = self.select(
+                "SELECT n.nick, COUNT(l.url) AS count FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC LIMIT 10")
         else:
-            data = self.select("SELECT n.nick, COUNT(l.url) AS count FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC")
+            data = self.select(
+                "SELECT n.nick, COUNT(l.url) AS count FROM cardboardlinks l, cardboardnick n WHERE l.name = n.jid GROUP BY l.name ORDER BY count DESC")
         return data
 
     def get_domains_by_links(self, limit=False, user=None):
@@ -87,7 +102,7 @@ class CardboardData():
             query = query + " LIMIT 10"
         data = self.select(query)
         return data
-        
+
     def get_link_counts(self, user=None):
         query = "SELECT COUNT(url) FROM cardboardlinks"
         param = None
@@ -100,14 +115,14 @@ class CardboardData():
 
     def get_log_counts(self, user=None):
         query = "SELECT COUNT(message) FROM cardboardlog"
-        param=None
+        param = None
         if user:
             query = query + " WHERE name=?"
             param = (user, )
         data = self.select(query, param)
         count = data[0][0]
         return count
-        
+
     def get_nick(self, user=None):
         if not user:
             return None
@@ -115,7 +130,7 @@ class CardboardData():
         param = (user, )
         data = self.select(query, param)
         return data[0][0]
-        
+
     def select(self, query, param=None):
         db = sqlite3.connect('/home/wolfgang/cardboardenv/cardboardbot/cardboardlog.db')
         c = db.cursor()
