a589514fed2c0aa95dd1d9c44d2bfd74ae406a02
Wolfgang Vandevenne <woll0r@gmail.com>
2015-03-04 22:16:34 +0100

    Split off common graph stuff

---
+++ static/js/graphscommon.js
@@ -0,0 +1,19 @@
+// Common stuff for graphs
+
+var options = {
+	series: {
+		color: "#007ba0",
+		bars: {
+			show: true,
+			align: "center",
+			barWidth: 0.8,
+			fillColor: "#008CBA"
+		}
+	},
+	xaxis: {
+		mode: "categories",
+		ticklength: 0,
+		rotateTicks: 45,
+		autoscaleMargin: 0.01
+	}
+};
